6df6cbf3ae44313da78a71192d7292ce8e52675f
Kunal-mob <102619984+Kunal-mob@users.noreply.github.com>
2026-06-07 22:19:04 +0530

    fix: update import path for StoreContext in MyOrders component refactor: comment out unused multer storage configuration in foodroute

--- backend/routes/foodroute.js
+++ backend/routes/foodroute.js
@@ -6,16 +6,16 @@ import { listfood } from "../controllers/foodcontroller.js";
 const foodRouter = express.Router();
 
 //Image storage engine
-const storage = multer.diskStorage({
-    destination:'uploads',
-    filename:(req,file,cb)=>{
-return cb(null,`${Date.now()}${file.originalname}`)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination:'uploads',
+//     filename:(req,file,cb)=>{
+// return cb(null,`${Date.now()}${file.originalname}`)
+//     }
+// })
 
 const upload = multer({storage:storage});
 
-foodRouter.post('/add', upload.single('image'), addFood);
+// foodRouter.post('/add', upload.single('image'), addFood);
 foodRouter.get('/list', listfood);
 foodRouter.post('/remove', removefood);
 
